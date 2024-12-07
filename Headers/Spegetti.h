660103b5a59f4a55a5c0cc089e8e3a1f5293eb1f
AlexIsCool321 <alex.m.diehl1@gmail.com>
2024-12-06 23:34:10 -0600

    Project Set Up

---
+++ Headers/Spegetti.h
@@ -0,0 +1,14 @@
+#ifndef Spegetti_H
+#define Spegetti_H
+
+#include <iostream>
+
+namespace Spegetti
+{
+	
+	void Init();
+	
+}
+
+
+#endif Spegetti_H
---
+++ Sources/Spegetti.cpp
@@ -0,0 +1,13 @@
+#include <Headers/Spegetti.h>
+
+namespace Spegetti
+{
+	
+	void Init()
+	{
+		
+		std::cout << "Hello, World!" << std::endl;
+		
+	}
+	
+}
